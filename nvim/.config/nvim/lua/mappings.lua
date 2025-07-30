96abddbb7bc6224092523879b9444e90e2baff9e
vanajmoorthy <vanajmoorthy@gmail.com>
2025-07-30 21:01:00 +0530

    feat: add keybinds to delete all python and js comments

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -5,8 +5,14 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+
 map("i", "jk", "<ESC>")
+
 map("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving down half-page" })
+
+map("n", "<leader>d#", [[:%s/#.*//g<CR>]], { desc = "Delete all inline Python comments" })
+
+map("n", "<leader>d/", [[:silent! %s#//.*$##g<CR>]], { desc = "Delete all inline JS comments (//...)" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 map("n", "<leader>fm", function()
