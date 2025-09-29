d367c99e22e066de0fc4a45898d855bf1e15edaa
Vanaj Moorthy <vanajmoorthy@gmail.com>
2025-09-29 17:54:38 +0100

    new colorscheme and remove offsets for nvim tree

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,17 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	priority = 1000,
+	"sainnhe/gruvbox-material",
+	lazy = false, -- Make sure the theme is loaded on startup
+	priority = 1000, -- Load it before other plugins
 	config = function()
-		vim.cmd.colorscheme("gruvbox")
+		-- Set the theme variant you want to use
+		-- Options: "hard", "medium", "soft"
+		vim.g.gruvbox_material_background = "medium"
+
+		-- You can also change the contrast for more vibrancy
+		-- Options: "high", "medium", "low" (default is "medium")
+		vim.g.gruvbox_material_contrast = "high"
+
+		-- Load the colorscheme
+		vim.cmd.colorscheme("gruvbox-material")
 	end,
 }
--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,140 +1,132 @@
 -- ~/.config/nvim/lua/plugins/ui.lua
 
 return {
-  -- File explorer
-  {
-    'nvim-tree/nvim-tree.lua',
-    version = "*",
-    lazy = false,
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-      vim.opt.fillchars = { eob = " " }
-      local function on_attach(bufnr)
-        local api = require('nvim-tree.api')
+	-- File explorer
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			vim.opt.fillchars = { eob = " " }
+			local function on_attach(bufnr)
+				local api = require("nvim-tree.api")
 
-        local function opts(desc)
-          return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-        end
+				local function opts(desc)
+					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				end
 
-        api.config.mappings.default_on_attach(bufnr)
+				api.config.mappings.default_on_attach(bufnr)
 
-        vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
-        vim.keymap.set('n', 'o', api.node.open.tab, opts('Open: New Tab'))
-      end
+				vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+				vim.keymap.set("n", "o", api.node.open.tab, opts("Open: New Tab"))
+			end
 
-      require('nvim-tree').setup({
-        sort_by = "case_sensitive",
-        on_attach = on_attach,
-        update_focused_file = {
-          enable = true,
-          update_cwd = true,
-        },
-        view = {
-          width = 30,
-          side = "left",
-        },
-        renderer = {
-          group_empty = true,
-          root_folder_modifier = ":t",
-          indent_markers = {
-            enable = false,
-          },
-          icons = {
-            show = {
-              file = true,
-              folder = true,
-              folder_arrow = true,
-              git = true,
-            },
-            glyphs = {
-              git = {
-                unstaged = "✗",
-                staged = "✓",
-                unmerged = "",
-                renamed = "➜",
-                untracked = "★",
-                deleted = "",
-                ignored = "◌",
-              },
-            },
-          },
-          highlight_git = true
-        },
-        filters = {
-          dotfiles = false, -- Show dotfiles
-        },
-        actions = {
-          open_file = {
-            quit_on_open = false,
-            window_picker = {
-              enable = false,
-            },
-          }
-        },
-        -- Disable netrw at the very start of init.lua
-        disable_netrw = true,
-        hijack_netrw = true,
-      })
-      vim.cmd([[
+			require("nvim-tree").setup({
+				sort_by = "case_sensitive",
+				on_attach = on_attach,
+				update_focused_file = {
+					enable = true,
+					update_cwd = true,
+				},
+				view = {
+					width = 30,
+					side = "left",
+				},
+				renderer = {
+					group_empty = true,
+					root_folder_modifier = ":t",
+					indent_markers = {
+						enable = false,
+					},
+					icons = {
+						show = {
+							file = true,
+							folder = true,
+							folder_arrow = true,
+							git = true,
+						},
+						glyphs = {
+							git = {
+								unstaged = "✗",
+								staged = "✓",
+								unmerged = "",
+								renamed = "➜",
+								untracked = "★",
+								deleted = "",
+								ignored = "◌",
+							},
+						},
+					},
+					highlight_git = true,
+				},
+				filters = {
+					dotfiles = false, -- Show dotfiles
+				},
+				actions = {
+					open_file = {
+						quit_on_open = false,
+						window_picker = {
+							enable = false,
+						},
+					},
+				},
+				-- Disable netrw at the very start of init.lua
+				disable_netrw = true,
+				hijack_netrw = true,
+			})
+			vim.cmd([[
         highlight NvimTreeGitDirty guifg=#fb4934
         highlight NvimTreeGitStaged guifg=#b8bb26
         highlight NvimTreeGitNew guifg=#fabd2f
         highlight NvimTreeGitDeleted guifg=#fb4934
       ]])
-    end,
-  },
-  -- Icons
-  { 'nvim-tree/nvim-web-devicons' },
+		end,
+	},
+	-- Icons
+	{ "nvim-tree/nvim-web-devicons" },
 
-  -- Tabline
-  {
-    'akinsho/bufferline.nvim',
-    event = 'VimEnter',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
-      'tiagovla/scope.nvim', -- Add scope as a dependency
-    },
-    config = function()
-      require('bufferline').setup({
-        options = {
-          mode = "buffers",
-          separator_style = "slant",
-          show_buffer_close_icons = true,
-          show_close_icon = true,
-          offsets = {
-            {
-              filetype = "NvimTree",
-              text = "",
-              highlight = "Directory",
-              text_align = "left",
-            }
-          },
-          custom_filter = function(buf_number)
-            -- Simple filter - scope.nvim handles tab-scoping automatically
-            if vim.api.nvim_buf_get_name(buf_number) ~= "" then
-              local filetype = vim.bo[buf_number].filetype
-              if filetype ~= "NvimTree" and filetype ~= "snacks_picker_list" then
-                return true
-              end
-            end
-            return false
-          end,
-        }
-      })
-    end,
-  },
+	-- Tabline
+	{
+		"akinsho/bufferline.nvim",
+		event = "VimEnter",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"tiagovla/scope.nvim", -- Add scope as a dependency
+		},
+		config = function()
+			require("bufferline").setup({
+				options = {
+					mode = "buffers",
+					separator_style = "slant",
+					show_buffer_close_icons = true,
+					show_close_icon = true,
+					custom_filter = function(buf_number)
+						-- Simple filter - scope.nvim handles tab-scoping automatically
+						if vim.api.nvim_buf_get_name(buf_number) ~= "" then
+							local filetype = vim.bo[buf_number].filetype
+							if filetype ~= "NvimTree" and filetype ~= "snacks_picker_list" then
+								return true
+							end
+						end
+						return false
+					end,
+				},
+			})
+		end,
+	},
 
-  -- Statusline
-  { 'echasnovski/mini.statusline',         event = 'VimEnter', config = true },
+	-- Statusline
+	{ "echasnovski/mini.statusline", event = "VimEnter", config = true },
 
-  -- Indent guides
-  { 'lukas-reineke/indent-blankline.nvim', main = 'ibl',       event = { 'BufReadPost', 'BufNewFile' }, opts = {} },
+	-- Indent guides
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", event = { "BufReadPost", "BufNewFile" }, opts = {} },
 
-  -- Marks
-  { 'chentoast/marks.nvim',                event = 'VimEnter', config = true },
+	-- Marks
+	{ "chentoast/marks.nvim", event = "VimEnter", config = true },
 
-  -- Optional: Minimap
-  { 'echasnovski/mini.map',                event = 'VimEnter', config = true },
+	-- Optional: Minimap
+	{ "echasnovski/mini.map", event = "VimEnter", config = true },
 }
